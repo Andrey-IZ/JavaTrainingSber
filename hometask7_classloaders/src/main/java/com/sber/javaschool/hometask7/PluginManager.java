36a0298209236bad9baa2061962b030977467ed8
Andrew <Andrey.zaporozhtsev@yandex.ru>
2021-09-19 23:21:02 +0300

    hometask7: fix task1

--- hometask7_classloaders/src/main/java/com/sber/javaschool/hometask7/PluginManager.java
+++ hometask7_classloaders/src/main/java/com/sber/javaschool/hometask7/PluginManager.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.lang.reflect.InvocationTargetException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
 
 public class PluginManager {
     private final String pluginRootDirectory;
@@ -12,9 +13,23 @@ public class PluginManager {
         this.pluginRootDirectory = pluginRootDirectory;
     }
 
-    public Plugin load(String pluginName, String pluginClassName) throws MalformedURLException, ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
-        URL[] urls = new URL[]{new URL(String.join(File.pathSeparator, pluginRootDirectory, pluginName))};
+    public List<String> getPluginPaths() {
+        return null;
+    }
+
+    public Plugin load(String pluginName, String pluginClassName) throws MalformedURLException, InstantiationException {
+        var path = String.join(File.separator, pluginRootDirectory, pluginName);
+
+        URL[] urls = new URL[]{new File(path).toURI().toURL()};
         MyClassLoader classLoader = new MyClassLoader(urls);
-        return (Plugin) classLoader.loadClass(pluginClassName).getDeclaredConstructor().newInstance();
+        try {
+            return (Plugin) classLoader.loadClass(pluginClassName).getDeclaredConstructor().newInstance();
+        } catch (InstantiationException | NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            System.out.println("The default constructor hasn't been found");
+            throw new InstantiationException(e.getMessage());
+        } catch (ClassNotFoundException e) {
+            System.out.println("The class hasn't been found");
+        }
+        return null;
     }
 }
