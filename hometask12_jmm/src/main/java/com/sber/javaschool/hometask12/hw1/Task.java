a48fcfda7638f169fa8dcffd879b8cd35fdf61ea
Andrew <Andrey.zaporozhtsev@yandex.ru>
2021-10-01 23:21:44 +0300

    hometask12: add custom exception

--- hometask12_jmm/src/main/java/com/sber/javaschool/hometask12/hw1/Task.java
+++ hometask12_jmm/src/main/java/com/sber/javaschool/hometask12/hw1/Task.java
@@ -2,6 +2,24 @@ package com.sber.javaschool.hometask12.hw1;
 
 import java.util.concurrent.Callable;
 
+/**
+ * Данный класс в конструкторе принимает экземпляр java.util.concurrent.Callable.
+ * Callable похож на Runnuble, но результатом его работы является объект (а не void).
+ * <p>
+ * Ваша задача реализовать метод get() который возвращает результат работы Callable.
+ * Выполнение callable должен начинать тот поток, который первый вызвал метод get().
+ * <p>
+ * Если несколько потоков одновременно вызывают этот метод, то выполнение должно начаться
+ * только в одном потоке, а остальные должны ожидать конца выполнения (не нагружая процессор).
+ * Если при вызове get() результат уже просчитан, то он должен вернуться сразу,
+ * (даже без задержек на вход в синхронизированную область).
+ * <p>
+ * Если при просчете результата произошел Exception, то всем потокам при вызове get(),
+ * надо кидать этот Exception, обернутый в ваш RuntimeException (подходящее название своему
+ * ексепшену придумайте сами).
+ *
+ * @param <T>
+ */
 public class Task<T> {
     private final Callable<? extends T> callable;
 
@@ -9,8 +27,13 @@ public class Task<T> {
         this.callable = callable;
     }
 
-    public T get() throws Exception {
+    public T get() throws TaskException {
         // TODO
-        return callable.call();
+        try {
+            return callable.call();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
