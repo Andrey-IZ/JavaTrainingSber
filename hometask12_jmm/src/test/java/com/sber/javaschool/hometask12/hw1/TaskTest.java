788e6158f5eb0bba704f9152f4d2bc8a0841eefc
Andrew <Andrey.zaporozhtsev@yandex.ru>
2021-10-02 22:09:49 +0300

    hometask12, part1: add second test

--- hometask12_jmm/src/test/java/com/sber/javaschool/hometask12/hw1/TaskTest.java
+++ hometask12_jmm/src/test/java/com/sber/javaschool/hometask12/hw1/TaskTest.java
@@ -9,8 +9,7 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class TaskTest {
 
@@ -23,17 +22,18 @@ class TaskTest {
         var resultsList = new ConcurrentLinkedQueue<Double>();
         var atomicInteger = new AtomicInteger();
         Task<Double> task = new Task<>(() -> {
-            TimeUnit.MILLISECONDS.sleep(100);
+            TimeUnit.MILLISECONDS.sleep(1);
             atomicInteger.incrementAndGet();
             return 33.0;
         });
         List<Thread> threadList = new ArrayList<>();
-        int amountThreads = 10;
+        int amountThreads = 100;
 
         for (int i = 0; i < amountThreads; i++) {
             Thread thread = new Thread(() -> {
                 try {
                     var result = task.get();
+                    assertNotNull(result);
                     resultsList.add(result);
                 } catch (TaskException e) {
                     e.printStackTrace();
@@ -52,4 +52,32 @@ class TaskTest {
         assertTrue(resultsList.stream().allMatch(element::equals));
     }
 
+    @Test
+    void testWIthException() throws InterruptedException {
+        ConcurrentLinkedQueue<Throwable> thrownCauseList = new ConcurrentLinkedQueue<>();
+        Thread.setDefaultUncaughtExceptionHandler((thread, throwable) -> {
+            if (throwable instanceof TaskException) {
+                thrownCauseList.add(throwable);
+            }
+        });
+
+        Task<Double> task = new Task<>(() -> {
+            throw new TaskException("has happened the problem");
+        });
+        List<Thread> threadList = new ArrayList<>();
+        int amountThreads = 100;
+
+        for (int i = 0; i < amountThreads; i++) {
+            Thread thread = new Thread(task::get);
+            thread.start();
+            threadList.add(thread);
+        }
+
+        for (Thread thread : threadList) {
+            thread.join();
+        }
+        assertEquals(amountThreads, thrownCauseList.size(), "There mustn't be any assertion errors");
+        var firstElem = thrownCauseList.peek();
+        assertTrue(thrownCauseList.stream().allMatch(throwable -> throwable.equals(firstElem)), "size: " + thrownCauseList);
+    }
 }
